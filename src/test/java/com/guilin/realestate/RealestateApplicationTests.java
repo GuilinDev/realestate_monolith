7988645f37906c44ca829bf184da8c24acb9b578
guilindev <guilindev@gmail.com>
2019-01-30 18:23:55 -0500

    real estate platform monolith initial checkin

---
+++ src/test/java/com/guilin/realestate/RealestateApplicationTests.java
@@ -0,0 +1,17 @@
+package com.guilin.realestate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RealestateApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
